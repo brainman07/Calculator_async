1beed09ffb23f3c43ccb532b29b2c90494dae66d
brainman07 <weaselwiz7@gmail.com>
2017-11-18 23:31:30 +0200

    added .css file

--- calculate.js
+++ calculate.js
@@ -1,3 +1,24 @@
 function calculate(){
+	var dropdown = document.getElementById("dropdown1");
 	
+	var	nr1 = document.getElementById("firstNumber").value;
+	window.alert(nr1);
+
+	var	nr2 = document.getElementById("firstNumber").value;
+	var result = 0;
+
+	switch (dropdown.selectedIndex){
+		case 0:
+			result = nr1 + nr2;
+		case 1:
+			result = nr1 - nr2;
+		case 2:
+			result = nr1 * nr2;
+		case 3:
+			result = nr1 / nr2;
+		case 4:
+			result = Math.pow(nr1, nr2);
+	}
+
+	document.getElementById("result").value = result;
 }
