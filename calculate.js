41c41338854e1aa1adb74fbd43713d86aeb9234b
brainman07 <weaselwiz7@gmail.com>
2017-11-19 00:29:53 +0200

    edited .js file

--- calculate.js
+++ calculate.js
@@ -1,24 +1,30 @@
-function calculate(){
+function operation(){
 	var dropdown = document.getElementById("dropdown1");
-	
-	var	nr1 = document.getElementById("firstNumber").value;
-	window.alert(nr1);
 
-	var	nr2 = document.getElementById("firstNumber").value;
+	var	nr1 = document.getElementById("firstNumber").value;
+
+	var	nr2 = document.getElementById("secondNumber").value;
 	var result = 0;
 
 	switch (dropdown.selectedIndex){
 		case 0:
-			result = nr1 + nr2;
+			result = (+nr1) + (+nr2);
+
+			break;
 		case 1:
-			result = nr1 - nr2;
+			result = (+nr1) - (+nr2);
+			break;
 		case 2:
-			result = nr1 * nr2;
+			result = (+nr1) * (+nr2);
+			break;
 		case 3:
-			result = nr1 / nr2;
+			result = (+nr1) / (+nr2);
+			break;
 		case 4:
-			result = Math.pow(nr1, nr2);
+			result = Math.pow((+nr1), (+nr2));
+			break;
 	}
 
-	document.getElementById("result").value = result;
+	
+	document.getElementById("result").innerHTML = result;
 }
