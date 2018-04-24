4ee9c116f2bc054e946c9aacfde3f45a439a1a01
brainman07 <weaselwiz7@gmail.com>
2018-04-25 00:46:24 +0300

    jquery experiments

--- calculate.js
+++ calculate.js
@@ -1,44 +1,50 @@
-function get_numbers(){
-	var	firstNumber = document.getElementById("firstNumber").value;
-	var	secondNumber = document.getElementById("secondNumber").value;
-	var values = {
-		nr1:(+firstNumber), 
-		nr2:(+secondNumber)
-	};
-	return values;
+function getNumbers()
+{
+    var firstNumber = $("#firstNumber").val();
+    //console.log(typeof $("#firstNumber"));
+    var secondNumber = $("#secondNumber").val();
+    var values = 
+    {
+        nr1:(+firstNumber), 
+        nr2:(+secondNumber)
+    };
+    return values;
 }
 
-function get_operation(){
-	var dropdown = document.getElementById("dropdown1");
-	return dropdown.options[dropdown.selectedIndex].value;
+function getOperation()
+{
+    var option = $("#dropdown1 option:selected");
+    //return dropdown.options[dropdown.selectedIndex].value;
+    console.log($("#dropdown1 option:selected").text());
+    return $("#dropdown1 option:selected").val();
 }
 
-function execute_operation(){
-	
-	var values = get_numbers();
-	var result = 0;
+function executeOperation()
+{
+    var values = getNumbers();
+    var result = 0;
 
-	switch (get_operation()){
-		case "Addition":
-			result = (values.nr1) + (values.nr2);
-			break;
+    switch (getOperation()){
+        case "Addition":
+            result = (values.nr1) + (values.nr2);
+            break;
 
-		case "Subtraction":
-			result = (values.nr1) - (values.nr2);
-			break;
+        case "Subtraction":
+            result = (values.nr1) - (values.nr2);
+            break;
 
-		case "Multiplication":
-			result = (values.nr1) * (values.nr2);
-			break;
+        case "Multiplication":
+            result = (values.nr1) * (values.nr2);
+            break;
 
-		case "Division":
-			result = (values.nr1) / (values.nr2);
-			break;
+        case "Division":
+            result = (values.nr1) / (values.nr2);
+            break;
 
-		case "Pow":
-			result = Math.pow((values.nr1), (values.nr2));
-			break;
-	}
+        case "Pow":
+            result = Math.pow((values.nr1), (values.nr2));
+            break;
+    }
 
-	document.getElementById("result").innerHTML = result;
+    $("#result").html(result);
 }
