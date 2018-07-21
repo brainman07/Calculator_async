6b88f81dcf068de74861137cc777db6f83d458e7
brainman07 <weaselwiz7@gmail.com>
2018-07-21 14:49:47 +0300

    async sql operations started

--- backend/SqlHistoryStorage.js
+++ backend/SqlHistoryStorage.js
@@ -6,48 +6,49 @@ module.exports = class SqlHistoryStorage {
     }
 
     createConnection() {
-        this.con = mysql.createConnection(this.db);
+        con = mysql.createConnection(this.db);
 
-        this.con.connect((err) => {
+        con.connect((err) => {
             if (err) {
                 console.log('Error connecting to the db.');
             };
         });
+        return con;
     }
 
     async saveHistoryEntry(entry) {
-        this.createConnection();
         const query =  `INSERT INTO history 
                         (Operation, Number1, Number2, Result, Time_stamp) 
                         VALUES ('${entry.operation}', ${entry.number1}, ${entry.number2}, ${entry.result}, 
                         '${entry.timestamp.getFullYear()}-${entry.timestamp.getMonth()+1}-${entry.timestamp.getDate()} ${entry.timestamp.getHours()}:${entry.timestamp.getMinutes()}:${entry.timestamp.getSeconds()}')`;
-        return new Promise( (resolve, reject) => {
-            try {
-                await this.con.query(query);
-            }
-            catch {
-                console.log("Error executing saveHistoryEntry query.");
-            }
-            this.con.end();
-            resolve();
+                        
+        return new Promise(async (resolve, reject) => {
+            const con = await this.createConnection();
+            con.query(query, (result, err) => {
+                con.end();
+                if (err) {
+                    reject("Error executing saveHistoryEntry query.");
+                }
+                else {
+                    resolve(result);
+                }
+            });
         });
     }
 
     async getHistory() {
-        this.createConnection();
         const query = "SELECT * FROM history";
-        var history;
 
-        return new Promise( (resolve, reject) => {
-            this.con.query(query, (err, result) => {
+        return new Promise(async (resolve, reject) => {
+            const con = await this.createConnection();
+            con.query(query, (result, err) => {
+                con.end();
                 if (err) {
-                    console.log(err);
+                    reject("Error executing getHistory query.");
                 } else {
-                    history = result;
+                    resolve(result);
                 };
             });
-            this.con.end();
-            resolve(history);
         });
     }
 }
