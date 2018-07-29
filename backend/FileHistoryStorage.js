53706148ac0092d34d41504b47145219d945f1fc
brainman07 <weaselwiz7@gmail.com>
2018-07-29 22:35:04 +0300

    FileHistoryStorage finished

--- backend/FileHistoryStorage.js
+++ backend/FileHistoryStorage.js
@@ -1,17 +1,21 @@
 const HistoryEntry = require('./HistoryEntry');
 const fs = require('fs').promises;
 
-var history = [];
-
 module.exports = class FileHistoryStorage {
     constructor(filePath) {
         this.filePath = filePath;
-        fs.writeFile(filePath, '');
+        fs.appendFile(filePath, '');
     }
 
     async saveHistoryEntry(entry) {
+        const jsonBuffer = await fs.readFile(this.filePath);
+        const json = jsonBuffer.toString();
+
+        var history = json ? JSON.parse(json) : [];
+
         history.push(entry);
-        await fs.writeFile(this.filePath, JSON.stringify(history) + "\n");
+
+        await fs.writeFile(this.filePath, JSON.stringify(history));
     }
 
     async getHistory() {
